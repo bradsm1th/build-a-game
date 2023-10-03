77f91672c7b07f5c04db0947db84b343d3cd43e8
Brad Smith <bradsmith@duck.com>
2023-10-03 11:46:10 -0400

    Add part 2 code

--- js/main.js
+++ js/main.js
@@ -6,6 +6,19 @@ const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-coun
 
 /*----- app's state (variables) -----*/
 
+// {}. 
+// keys:    'p' == player, 't' == tie, 'c' == computer
+// values:  0,1,2…n
+let scores;
+
+// {}. 
+// keys:    'p'layer, 'c'omputer
+// values:  'r'ock, 'p'aper, 's'cissors
+let results;
+
+// ''.
+// 'p'layer, 't'ie, 'c'omputer
+let outcome;
 
 /*----- cached element references -----*/
 
@@ -14,4 +27,27 @@ const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-coun
 
 
 /*----- functions -----*/
+init();
 
+// initialize all state and call render()
+function init() {
+  scores = {
+    p: 0,
+    t: 0,
+    c: 0,
+  };
+
+  results = {
+    p: 'r',
+    c: 'r',
+  };
+
+  outcome = 't';
+
+  render();
+
+}
+
+function render() {
+  
+}
